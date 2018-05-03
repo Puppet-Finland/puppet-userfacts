1033530819dff536e0c86413a822875bbe673775
Samuli Seppänen <samuli.seppanen@gmail.com>
2018-05-03 20:21:06 +0300

    Try to make Ruby style checker happy
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- lib/facter/localusers.rb
+++ lib/facter/localusers.rb
@@ -3,7 +3,7 @@ if Facter.value(:kernel) != 'windows'
 
   Etc.passwd do |entry|
     # Normal users have IDs in this range in /etc/login.defs
-    if entry.uid >= 1000 and entry.uid <= 60000
+    if entry.uid >= 1_000 && entry.uid <= 60_000
       Facter.add("user_#{entry.name}_is_present") do
         setcode do
           true
